6265a0bd6f9e93723a9fec253473cab8d4187b01
mirfankmrjk <89370534+m-irfan-km-rjk@users.noreply.github.com>
2024-10-06 16:19:07 +0530

    Update config.js
    Privatize firebaze details

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuxjAsQ6t-kQ5-zeZCjmspPtnNsWssjIM",
-  authDomain: "mandrake-86624.firebaseapp.com",
-  projectId: "mandrake-86624",
-  storageBucket: "mandrake-86624.appspot.com",
-  messagingSenderId: "571666797439",
-  appId: "1:571666797439:web:fe2e0cf6360eeb3066404a"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
